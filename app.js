4a7e5aca4a4f3428a1620aaa7b9e44a224ee5a19
Aswin R C <130204954+rcaswin@users.noreply.github.com>
2024-07-09 20:10:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".word",{
+    strings:["Full Stack Developer", "Mobile App Developer", "UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
