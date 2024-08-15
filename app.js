01e78626e19d521d838ca4409a44951cdd7bb628
rcaswin <rcaswin6@gmail.com>
2024-08-15 19:34:29 +0530

    submit

--- app.js
+++ app.js
@@ -6,4 +6,33 @@ var typed = new Typed(".word",{
     loop:true
 });
 
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY+97<section[len].offsetTop){}
+    menuLi.forEach(sec=>sec.classList.remove("active"));
+    menuLi[len].classList.add("active")
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    window.classList.toggle("sticky",window.scrollY>50)
+})
+
+let menuIcon = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle("bx-x");
+    navList.classList.toggle("open");
+}
+
+window.onscroll=()=>{
+    menuIcon.classList.remove("bx-x");
+    navList.classList.remove("open");
+}
 
