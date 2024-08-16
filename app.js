be40cb893592017a33b14b0a52fb570e955b411a
rcaswin <rcaswin6@gmail.com>
2024-08-16 20:22:53 +0530

    submit

--- app.js
+++ app.js
@@ -36,3 +36,46 @@ window.onscroll=()=>{
     navList.classList.remove("open");
 }
 
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer(el));
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer(el));
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer(el));
+
+// js/contact.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        // Collect the form data
+        var templateParams = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            address: document.getElementById('address').value,
+            phone: document.getElementById('phone').value,
+            message: document.getElementById('message').value
+        };
+
+        // Send the email
+        emailjs.send('service_gx010d8', 'template_e9li1lo', templateParams)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert("Message sent successfully!");
+            }, function(error) {
+                console.error('FAILED...', error);
+                alert("Failed to send the message.");
+            });
+    });
+});
